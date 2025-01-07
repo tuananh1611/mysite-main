0d888c462fc38abdb5653d14f274e4387a5585aa
root <root@DESKTOP-VUMKCHF.>
2025-01-08 01:01:02 +0700

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  allow_unauthenticated_access only: %i[ new create ]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+
+  def new
+    unless User.exists?(email_address: 'admin@example.com')
+      User.create!(
+        email_address: 'admin@example.com',
+        password: '123',
+        password_confirmation: '123'
+      )
+      end
+  end
+
+  def create
+    if user = User.authenticate_by(params.permit(:email_address, :password))
+      start_new_session_for user
+      redirect_to after_authentication_url
+    else
+      redirect_to new_session_path, alert: "Try another email address or password."
+    end
+  end
+
+  def destroy
+    terminate_session
+    redirect_to new_session_path
+  end
+end
---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
